38df1ec07c2588511188fb51ed95ed450207f2e9
nikita <nikitanozhenko@gmail.com>
2022-01-16 15:30:49 +0200

    add docstring

--- HW15/main.py
+++ HW15/main.py
@@ -19,12 +19,27 @@ def get_db():
 
 @app.get("/first_source/", response_model=List[models.FirsSource])
 def read_first_source_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    This function return all news from first source
+
+    Parameters
+        skip : int
+        limit : int
+        db : database
+    """
     first_source_all = crud.get_first_source_all(db, skip=skip, limit=limit)
     return first_source_all
 
 
 @app.get("/first_source/{item_id}", response_model=models.FirsSource)
 def read_first_source_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    This function return one news with the specified id from first source
+
+    Parameters
+        item_id : int
+        db : database
+    """
     first_source_item = crud.get_first_source_item(db, item_id=item_id)
     if first_source_item is None:
         raise HTTPException(status_code=404, detail="Item first source not found")
@@ -33,12 +48,27 @@ def read_first_source_item(item_id: int, db: Session = Depends(get_db)):
 
 @app.get("/second_source/", response_model=List[models.SecondSource])
 def read_second_source_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    This function return all news from second source
+
+    Parameters
+        skip : int
+        limit : int
+        db : database
+    """
     second_source_all = crud.get_second_source_all(db, skip=skip, limit=limit)
     return second_source_all
 
 
 @app.get("/second_source/{item_id}", response_model=models.SecondSource)
 def read_second_source_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    This function return one news with the specified id from second source
+
+    Parameters
+        item_id : int
+        db : database
+    """
     second_source_item = crud.get_second_source_item(db, item_id=item_id)
     if second_source_item is None:
         raise HTTPException(status_code=404, detail="Item second source not found")
